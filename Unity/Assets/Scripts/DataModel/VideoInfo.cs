2919ff1e35984ee70ed7da2cf6f25f60432254fa
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-14 13:25:16 +0900

    JSON Parseする処理の作成

---
+++ Unity/Assets/Scripts/DataModel/VideoInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class VideoInfo
+{
+	public string video_id;
+	public string timestamp;
+	public string title;
+	public List<VideoFromat> formats;
+}
+
+public class VideoFromat{
+	public string url;
+	public string container;
+	public string audioEncoding;
+	public int audioBitrate;
+}
