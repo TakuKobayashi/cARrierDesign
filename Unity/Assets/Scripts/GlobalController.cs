f422cffd6091054d934f4848e0498e6a6cc063aa
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-14 12:17:05 +0900

    画像を送りつけたらうまく抽出してくれた。その処理を追加

--- Unity/Assets/Scripts/GlobalController.cs
+++ Unity/Assets/Scripts/GlobalController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
 
 public class GlobalController : SingletonBehaviour<GlobalController>
 {
@@ -9,7 +11,7 @@ public class GlobalController : SingletonBehaviour<GlobalController>
     // Start is called before the first frame update
     void Start()
     {
-        this.AppearVideo("https://r4---sn-3pm76n7r.googlevideo.com/videoplayback?expire=1563091841&ei=IY8qXd2ZD9KZs8IP2Ka78AQ&ip=153.246.168.76&id=o-AEjWxokTmCb-KplgySS09wjHhPscTReOWDKUKdLNfkM2&itag=22&source=youtube&requiressl=yes&mm=31%2C29&mn=sn-3pm76n7r%2Csn-3pm7sn76&ms=au%2Crdu&mv=m&mvi=3&pl=19&initcwndbps=1402500&mime=video%2Fmp4&ratebypass=yes&dur=140.759&lmt=1556630365107148&mt=1563070183&fvip=5&c=WEB&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=ALgxI2wwRAIgDUuqOaTexBsTLxjUre1z9ob4ymhpI45COdxjD2y7YSsCICBeqpvuVNJWka5smPd-OT3lN-QIgsh3JRrGtw2WdABE&lsparams=mm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHylml4wRQIhANKE6QSjLbvrGFuDy_2FlYVaLPlzyZYjOmisuUM6m3QEAiA0pLrROM-WAOzalsX08RnnqisVR8BPkvhk2Jalttynjw%3D%3D");
+        StartCoroutine(this.UploadFile());
     }
 
     // Update is called once per frame
@@ -21,5 +23,32 @@ public class GlobalController : SingletonBehaviour<GlobalController>
     public void AppearVideo(string videoUrl, GameObject appearObject = null)
     {
         videoController.AppearVideoPlane(videoUrl, appearObject);
+    }
+
+    IEnumerator UploadFile()
+    {
+		string fileName = "IMG_20190714_100544.jpg";
+		string filePath = Path.Combine(Application.persistentDataPath , fileName);
+        // 画像ファイルをbyte配列に格納
+        byte[] img = File.ReadAllBytes(filePath);
+
+        // formにバイナリデータを追加
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("image", img, fileName, "image/jpeg");
+        // HTTPリクエストを送る
+        UnityWebRequest request = UnityWebRequest.Post("http://192.168.43.141:5000", form);
+        yield return request.Send();
+
+        if (request.isNetworkError)
+        {
+            // POSTに失敗した場合，エラーログを出力
+            Debug.Log(request.error);
+        }
+        else
+        {
+            // POSTに成功した場合，レスポンスコードを出力
+            Debug.Log(request.responseCode);
+			Debug.Log(request.downloadHandler.text);
+		}
     }
 }
