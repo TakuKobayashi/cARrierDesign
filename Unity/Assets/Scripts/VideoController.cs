6f72059f529f34be1149505eaabf772483393295
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-07-14 11:14:11 +0900

    秘伝なタレを注入し、動画再生の一連のメソッドを作成した

---
+++ Unity/Assets/Scripts/VideoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoController : MonoBehaviour
+{
+    [SerializeField] private GameObject videoPlaneObject;
+    private List<VideoPlane> videoPlanes = new List<VideoPlane>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AppearVideoPlane(string videoUrl, GameObject appearParent = null)
+    {
+        GameObject parentObj = appearParent;
+        if (parentObj == null)
+        {
+            parentObj = this.gameObject;
+        }
+        VideoPlane videoPlane = ComponentUtil.InstantiateTo<VideoPlane>(parentObj, videoPlaneObject);
+        videoPlane.PlayVideoFromUrl(videoUrl);
+        videoPlane.OnFinished = () =>
+        {
+            Destroy(videoPlane.gameObject);
+            videoPlanes.Remove(videoPlane);
+        };
+        videoPlanes.Add(videoPlane);
+    }
+}
---
+++ Unity/Assets/Scripts/VideoPlane.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlane : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    private bool isFinished = false;
+
+    public Action OnFinished;
+
+    void Awake()
+    {
+        videoPlayer = this.GetComponent<VideoPlayer>();
+    }
+
+    public void PlayVideoFromUrl(string url)
+    {
+        isFinished = false;
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = url;
+        videoPlayer.Prepare();
+        videoPlayer.Play();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((ulong)videoPlayer.frame == videoPlayer.frameCount)
+        {
+            if (OnFinished != null && !isFinished)
+            {
+                isFinished = true;
+                OnFinished();
+            }
+            return;
+        }
+    }
+}
